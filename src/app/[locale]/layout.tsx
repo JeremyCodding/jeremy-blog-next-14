fe54f7b2b61f38fffe9be56ba8cf736b8ba4849f
Jeremy <jeremy.devpaulepereira@gmail.com>
2025-05-31 13:11:05 -0300

    Adding locale in project

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { NextIntlClientProvider } from 'next-intl'
+import { getMessages } from "next-intl/server";
+
+
+export const metadata: Metadata = {
+  title: {template: "%s | Jeremy Blog", default: "Home | Jeremy Blog"}
+};
+
+export default async function RootLayout({
+  children,
+  params: { locale }
+}: Readonly<{
+  children: React.ReactNode;
+  params: { locale: string}
+}>) {
+  const messages = await getMessages()
+
+  return (
+    <html lang={locale}>
+      <body className={`antialiased`}>
+        <NextIntlClientProvider messages={messages}>
+
+        {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
