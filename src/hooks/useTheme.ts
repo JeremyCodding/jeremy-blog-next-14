b75aee2d32cac4e2afe6562a7eb2b0dca403b642
Jeremy <86443767+JeremyCodding@users.noreply.github.com>
2025-06-03 10:44:23 -0300

    feat: creating theme hook

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,33 @@
+import { useThemeStore } from "@/stores/themeStore"
+
+export const useTheme = () => {
+    const  { theme, setTheme } = useThemeStore()
+
+    const toggleTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light'
+
+        setTheme(newTheme)
+        localStorage.setItem('theme', newTheme)
+
+        document.body.classList.remove(theme)
+        document.body.classList.add(newTheme)
+    }
+
+    const getSavedTheme = () => {
+        const savedTheme = localStorage.getItem('theme')
+
+        if (savedTheme === 'light' || savedTheme === 'dark') {
+            setTheme(savedTheme)
+            document.body.classList.add(savedTheme)
+        } else {
+            setTheme('light')
+            document.body.classList.add('light')
+        }
+    }
+
+    return {
+        theme,
+        toggleTheme,
+        getSavedTheme,
+    }
+}
