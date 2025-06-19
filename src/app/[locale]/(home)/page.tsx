9495c7da470347b717d22a860f3ec0eaec113011
Jeremy <86443767+JeremyCodding@users.noreply.github.com>
2025-06-19 13:06:19 -0300

    feat: create home page component and page

---
+++ src/app/[locale]/(home)/page.tsx
@@ -0,0 +1,13 @@
+import { getMyBlogs } from "@/server/admin/blogService"
+import HomePage from "@/components/pages/Home"
+const Home = async () => {
+    const { data: blogs } = await getMyBlogs()
+
+    return (
+        <HomePage
+            blogs={blogs}
+        />
+    )
+}
+
+export default Home
